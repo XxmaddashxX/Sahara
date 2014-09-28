b0d0470f9a2f34569b5098c7e2f562b0779b5d15
XxmaddashxX <gcashmore96@hotmail.co.uk>
2014-09-28 13:57:53 +0100

    Added Playebase class and player class. Movement added

--- Sahara/src/main/java/net/thesahara/engine/Sahara.java
+++ Sahara/src/main/java/net/thesahara/engine/Sahara.java
@@ -1,7 +1,20 @@
 package net.thesahara.engine;
 
 
-import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL11.GL_BLEND;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
+import static org.lwjgl.opengl.GL11.GL_ONE_MINUS_SRC_ALPHA;
+import static org.lwjgl.opengl.GL11.GL_PROJECTION;
+import static org.lwjgl.opengl.GL11.GL_SRC_ALPHA;
+import static org.lwjgl.opengl.GL11.glBlendFunc;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.opengl.GL11.glEnable;
+import static org.lwjgl.opengl.GL11.glLoadIdentity;
+import static org.lwjgl.opengl.GL11.glMatrixMode;
+import static org.lwjgl.opengl.GL11.glOrtho;
+import static org.lwjgl.opengl.GL11.glViewport;
 
 import java.util.concurrent.TimeUnit;
 
@@ -9,6 +22,7 @@ import net.thesahara.engine.displays.DisplaySplash;
 import net.thesahara.engine.render.TextureStorage;
 import net.thesahara.engine.util.TextureHandler;
 import net.thesahara.game.SaharaProps;
+import net.thesahara.game.player.Player;
 
 import org.lwjgl.LWJGLException;
 import org.lwjgl.input.Keyboard;
@@ -24,7 +38,7 @@ import org.lwjgl.opengl.GL11;
  */
 public class Sahara {
 
-	
+	public static Player player;
 	public Sahara(){
 		
 	}
@@ -46,7 +60,11 @@ public class Sahara {
 		initializeGL();
 		TextureStorage.loadTextures();
 		displaySplashs();
+		player = new Player("Test");
+		player.setPlayerX(50);
+		player.setPlayerY(50);
 		while (!Display.isCloseRequested()) {
+			
 			input();
 			update();
 			render();
@@ -73,14 +91,29 @@ public class Sahara {
 	private static void input(){
 		try {
 			Keyboard.create();
+			if(Keyboard.isKeyDown(Keyboard.KEY_D)){
+				player.moveRight();
+			}
+			if(Keyboard.isKeyDown(Keyboard.KEY_S)){
+				player.moveDown();
+			}
+			if(Keyboard.isKeyDown(Keyboard.KEY_A)){
+				player.moveLeft();
+			}
+			if(Keyboard.isKeyDown(Keyboard.KEY_W)){
+				player.moveUp();
+			}
+			
+			
 		} catch (LWJGLException e) {
 			e.printStackTrace();
 		}
+		
 	}
 	private static void render(){
 		glClear(GL_COLOR_BUFFER_BIT);
 		glLoadIdentity();
-		TextureHandler.drawTexture(TextureStorage.test_Texture, 20, 20);
+		TextureHandler.drawTexture(TextureStorage.test_Texture, player.getPlayerX(), player.getPlayerY());
 		
 		Display.update();
 	}
