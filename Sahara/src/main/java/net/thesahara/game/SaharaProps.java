4e264f563e72dc37bfe5fbf9157b5d8f1229a8b2
XxmaddashxX <gcashmore96@hotmail.co.uk>
2014-09-30 20:22:56 +0100

    Update to 0.0.3!! Started prototype of Name generator for Enemies and civilians

--- Sahara/src/main/java/net/thesahara/game/SaharaProps.java
+++ Sahara/src/main/java/net/thesahara/game/SaharaProps.java
@@ -3,4 +3,7 @@ package net.thesahara.game;
 public class SaharaProps {
 
 	public static final String VERSION  = "Alpha 0.0.2";
+	public static final String AUTHOR_CODE = "XxmaddashxX";
+	public static final String AUTHOR_TEXTURE =  "Captain Ironbeard";
+	public static final String AUTHOR_SOUND = "Klldarkness";
 }
